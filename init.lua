3d9368753e26e48ee409de4ff655f4fbc90f450d
Neanderthal <Sergey.Istomin@gmail.com>
2023-02-08 00:41:23 +0300

    Initial commit

--- init.lua
+++ init.lua
@@ -35,6 +35,12 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 vim.opt.clipboard = "unnamedplus"
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+vim.g.netrw_winsize = 80
+vim.g.netrw_preview = 1
 
 require('after.treesitter')
 require('after.mason')
